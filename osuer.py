67167fe9e635e7c1abaec1e8218f9665f522d73b
电棍老板 <89052130+mixueicecreamandtea@users.noreply.github.com>
2023-06-17 19:41:07 +0800

    Update osuer.py

--- osuer.py
+++ osuer.py
@@ -14,80 +14,8 @@ headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 #UA
 response = requests.request("GET", url, headers=headers )
 
-print("response")
-
-exit()
-
-"""response = requests.request("GET", url, headers=headers, params=querystring)
-
-response = requests.request("GET", url, headers=headers, params=querystring)
-
 data = json.loads(response.text)
 
-code = data["code"]
-
-update_time = data["updateTime"]
-
-fx_link = data["fxLink"]
-
-now_data = data["now"]
-
-obs_time = now_data["obsTime"]
-
-temp = now_data["temp"]
-
-feels_like = now_data["feelsLike"]
-
-icon = now_data["icon"]
-
-text = now_data["text"]
-
-wind360 = now_data["wind360"]
-
-wind_dir = now_data["windDir"]
-
-wind_scale = now_data["windScale"]
-
-wind_speed = now_data["windSpeed"]
-
-humidity = now_data["humidity"]
-
-precip = now_data["precip"]
-
-pressure = now_data["pressure"]
-
-vis = now_data["vis"]
-
-cloud = now_data["cloud"]
-
-dew = now_data["dew"]
-
-print("网页视图链接",fx_link)
-
-print("天气更新时间",obs_time)
-
-print("当前温度为",temp,"℃")
-
-print("体感温度",feels_like,"℃")
-
-print("天气状况",text)
-
-print("风向360角度",wind360,"°")
-
-print("风向",wind_dir)
-
-print("风力等级",wind_scale)
-
-print("风速",wind_speed,"公里/小时")
-
-print("相对湿度",humidity,"％")
-
-print("当前小时累计降水量",precip,"毫米")
-
-print("大气压强",pressure,"百帕")
-
-print("能见度",vis,"公里")
-
-print("云量",cloud,"％")
+obs_time = data["obsTime"]
 
 print("露点湿度",dew)"""
