c8926bb5e5d29878dfed1c76b864e9f2bf6bc75f
电棍老板 <89052130+mixueicecreamandtea@users.noreply.github.com>
2023-06-17 18:27:29 +0800

    Update osuer.py

--- osuer.py
+++ osuer.py
@@ -5,23 +5,18 @@ import json
 
 import os
 
-key = input("俄亥俄州立大学匙：")
-
-user = input("俄亥俄州立大学名称/身份：")
+key = input("俄亥俄州立大学钥匙：")#密钥  获取(有osu账号即可):https://osu.ppy.sh/home/account/edit
+user = input("俄亥俄州立大学播放器名称/身份：")
 
 url = "https://osu.ppy.sh/api/get_user?" + "k=" + key +" &u=" + user
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 EdgA/110.0.0.0"}
-
+#UA
 response = requests.request("GET", url, headers=headers )
 
 print("response")
 
 exit()
-
-url = "https://devapi.qweather.com/v7/weather/now"
-
-querystring = {"location":city_id,"key":key}
 
 """response = requests.request("GET", url, headers=headers, params=querystring)
 
